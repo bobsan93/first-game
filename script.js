499609d616d9e7b489b48c1f3e29de6424ab89f0
bobsan93 <johan.bobby@hotmail.se>
2022-12-28 11:38:59 +0100

    started working on making the work easier in the futere

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let story = {
+    attack: {
+        titel: "chapter 1",
+        story: "find it's self in a dark cave and cant see very much. what should do",
+        choices: [
+            
+        ]
+    }
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     let button = document.querySelector('#start-button')
     let input = document.querySelector('#name-input')
@@ -6,7 +18,26 @@ document.addEventListener('DOMContentLoaded', function() {
         content.innerHTML = `
         <h1>Dark cave</h1>
         <p>${input.value} find it's self in a dark cave and cant see very much. what should
-        ${input.value} do?</p>`
+        ${input.value} do?</p>
+        <div>
+        <input id ="radio1" type = "radio" name = "choices" />
+        <label for "radio1">Go Right</label>
+        </div>
+        <div>
+        <input id ="radio1" type = "radio" name = "choices"/>
+        <label for "radio1">Go Left</label>
+        </div>
+        <div>
+        <input id ="radio1" type = "radio" name = "choices" />
+        <label for "radio1">Go staright</label>
+        </div>
+        <div>
+        <input id ="radio1" type = "radio" name = choices />
+        <label for "radio1">look around</label>
+        </div>
+        <button>Confirme</button>
+         `
+        
     })
 })
 
