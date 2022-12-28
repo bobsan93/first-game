3f5eeeb7bdb02b802bbd37f6d278e30f440015c2
bobsan93 <johan.bobby@hotmail.se>
2022-12-28 10:37:15 +0100

    the text based will for now be with option in mind and not with text from user

---
+++ script.js
@@ -0,0 +1,11 @@
+function game(){
+    document.addEventListener('DOMContentLoaded', function() {
+        let button = document.querySelector('#start-button')
+        let input = document.querySelector('#name-input')
+        let content = document.querySelector('#content')
+        button.addEventListener('click', function() {
+            content.innerHTML
+        })
+    })
+}
+game()   
