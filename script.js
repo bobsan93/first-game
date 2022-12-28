95c4f5e7b923533e6c9acb2097ac62d46bce1a53
bobsan93 <johan.bobby@hotmail.se>
2022-12-28 10:51:51 +0100

    the basics for the adnveture in place. input for name and a change of the html

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function game(){
         let input = document.querySelector('#name-input')
         let content = document.querySelector('#content')
         button.addEventListener('click', function() {
-            content.innerHTML
+            content.innerHTML = `
+            <h1>Dark cave</h1>
+            <p>${input.value} find it's self in a dark cave and cant see very much. what should
+            ${input.value} do?</p>`
         })
     })
 }
