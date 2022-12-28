ca598b9c2d6abaf28cd97be39cab2ad48db380d0
bobsan93 <johan.bobby@hotmail.se>
2022-12-28 11:19:09 +0100

    removed the game function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function game(){
-    document.addEventListener('DOMContentLoaded', function() {
-        let button = document.querySelector('#start-button')
-        let input = document.querySelector('#name-input')
-        let content = document.querySelector('#content')
-        button.addEventListener('click', function() {
-            content.innerHTML = `
-            <h1>Dark cave</h1>
-            <p>${input.value} find it's self in a dark cave and cant see very much. what should
-            ${input.value} do?</p>`
-        })
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.querySelector('#start-button')
+    let input = document.querySelector('#name-input')
+    let content = document.querySelector('#content')
+    button.addEventListener('click', function() {
+        content.innerHTML = `
+        <h1>Dark cave</h1>
+        <p>${input.value} find it's self in a dark cave and cant see very much. what should
+        ${input.value} do?</p>`
     })
-}
-game()   
+})
+
+   
